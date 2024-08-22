ee21a508f19bddb90d3bddf976f79931ddf72258
GwenZi0u0 <a0918566336@gmail.com>
2024-08-22 10:30:56 +0800

    feat determine the winner and change color.

--- src/GameContext.jsx
+++ src/GameContext.jsx
@@ -6,10 +6,69 @@ export const GameContext = createContext(null);
 export const GameDispatchContext = createContext(null);
 
 const initialState = {
-  guesses: Array(6).fill(""),
+  guesses: [],
   currentGuess: "",
   currentRow: 0,
+  answer: "GAMES",
+  guessColor: [],
+  status: "play",
 };
+console.log(initialState.answer);
+
+function checkColor(currentGuess, answer) {
+  let tmpResult = Array(5).fill("bg-gray-500");
+  for (let i = 0; i < 5; i++) {
+    if (currentGuess[i] === answer[i]) {
+      tmpResult[i] = "bg-green-500";
+      answer[i] = null;
+    }
+  }
+  for (let i = 0; i < 5; i++) {
+    let index = answer.indexOf(currentGuess[i]);
+    if (index !== -1) {
+      answer[index] = null;
+      tmpResult[i] = "bg-yellow-500";
+    }
+  }
+  return tmpResult;
+}
+
+function checkGuess(state) {
+  if (state.currentGuess.length === 5 && state.status === "play") {
+    let input = state.currentGuess.split("");
+    let answer = state.answer.split("");
+
+    let color = checkColor([...input], [...answer]);
+    const isWin = input.every((_, index) => input[index] === answer[index]);
+
+    if (isWin) {
+      return {
+        ...state,
+        guesses: [...state.guesses, input],
+        currentGuess: input,
+        guessColor: [...state.guessColor, color],
+        status: "win",
+      };
+    }
+
+    return {
+      ...state,
+      guesses: [...state.guesses, input],
+      currentGuess: "",
+      currentRow: state.currentRow + 1,
+      guessColor: [...state.guessColor, color],
+      status: state.currentRow + 1 >= 6 ? "lose" : "play",
+    };
+  }
+  return state;
+}
+
+// function generateNewAnswer() {
+//   const words = ["HELLO", "WORLD", "CANDY", "GAMES", "APPLE"];
+//   const answer = words[Math.floor(Math.random() * words.length)];
+//   console.log(answer);
+//   return answer;
+// }
 
 export function GameProvider({ children }) {
   const [state, dispatch] = useReducer(gameReducer, initialState);
@@ -26,7 +85,7 @@ export function GameProvider({ children }) {
 function gameReducer(state, action) {
   switch (action.type) {
     case ACTIONS.ADD_LETTER:
-      if (state.currentGuess.length < 5) {
+      if (state.currentGuess.length < 5 && state.status === "play") {
         return {
           ...state,
           currentGuess: state.currentGuess + action.payload.toUpperCase(),
@@ -35,18 +94,28 @@ function gameReducer(state, action) {
       return state;
 
     case ACTIONS.REMOVE_LETTER:
-      return {
-        ...state,
-        currentGuess: state.currentGuess.slice(0, -1),
-      };
-
-    case ACTIONS.SUBMIT_GUESS:
-      if (state.currentGuess.length === 5) {
+      if (state.currentGuess.length > 0 && state.status === "play") {
         return {
           ...state,
-          guesses: [...state.guesses, state.currentGuess],
+          currentGuess: state.currentGuess.slice(0, -1),
+        };
+      }
+      return state;
+
+    case ACTIONS.SUBMIT_GUESS: {
+      const updatadState = checkGuess(state);
+      return updatadState;
+    }
+
+    case ACTIONS.RESET_GAME:
+      if (state.status === "win" || state.status === "lose") {
+        return {
+          guesses: [],
           currentGuess: "",
-          currentRow: state.currentRow + 1,
+          currentRow: 0,
+          answer: "GAMES",
+          guessColor: [],
+          status: "play",
         };
       }
       return state;
